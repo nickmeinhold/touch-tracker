a3a08c18c54899d0da7ff7706b0e2bd8dfa3f5ac
Nick Meinhold <nick.meinhold@gmail.com>
2014-04-04 13:00:02 +1100

    Initial Commit

---
+++ TouchTracker/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  TouchTracker
+//
+//  Created by Nick Meinhold on 4/04/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchTracker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TouchTracker
+//
+//  Created by Nick Meinhold on 4/04/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
